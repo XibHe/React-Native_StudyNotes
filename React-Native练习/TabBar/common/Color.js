48a46945d40e00119951dbc95a02f912500443c7
Peng he <944588470@qq.com>
2018-12-04 21:21:01 +0800

    TabBarItem
    电影列表切换的TabBar

---
+++ React-Native练习/TabBar/common/Color.js
@@ -0,0 +1,8 @@
+// 用来存放app中需要用到的色值
+
+
+export default {
+    themeColor: '#268dcd',
+    border: '#e0e0e0',
+    background: '#f3f3f3'
+}
---
+++ React-Native练习/TabBar/scene/MovieListScreen.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/self-closing-comp */
+
+
+import React, {
+    Component
+} from 'react'
+
+import {
+    View
+} from 'react-native'
+
+export default class MovieListScreen extends Component {
+
+    render(){
+        return(
+          <View></View>
+        )
+    }
+}
